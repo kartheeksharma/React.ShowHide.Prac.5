65388dc972af85439deef754c1dafeecec56db90
kartheek03 <kartheeksharma03@gmail.com>
2024-01-12 11:58:09 +0000

    first commit

---
+++ src/components/ShowHide/index.js
@@ -0,0 +1,53 @@
+// Write your code here
+
+import {Component} from 'react'
+import './index.css'
+
+class ShowHide extends Component {
+  state = {
+    showFirstName: false,
+    showLastName: false,
+  }
+
+  onShowFirstName = () => {
+    this.setState(prevState => ({showFirstName: !prevState.showFirstName}))
+  }
+
+  onShowLastName = () => {
+    this.setState(prevState => ({showLastName: !prevState.showLastName}))
+  }
+
+  render() {
+    const {showFirstName, showLastName} = this.state
+
+    return (
+      <div className="app-container">
+        <h1 className="heading">Show/Hide</h1>
+        <div className="show-hide-cont">
+          <div className="name-cont">
+            <button
+              type="button"
+              className="sh-btn"
+              onClick={this.onShowFirstName}
+            >
+              Show/Hide Firstname
+            </button>
+            {showFirstName && <p className="name">Joe</p>}
+          </div>
+          <div className="name-cont">
+            <button
+              type="button"
+              className="sh-btn"
+              onClick={this.onShowLastName}
+            >
+              Show/Hide Lastname
+            </button>
+            {showLastName && <p className="name">Jonas</p>}
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default ShowHide
